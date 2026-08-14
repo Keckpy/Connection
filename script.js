f73e7137a6404010fd6d5f1b06144720ad455c86
Keckpy <36435107+Keckpy@users.noreply.github.com>
2026-08-14 18:31:12 -0400

    Set up modular architecture

---
+++ script.js
@@ -0,0 +1,29 @@
+const SUPABASE_URL = "https://wkjffzojirabmmyupqnc.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_U09_Lbeq10Bpfx4bm81TSA_lSe68d95";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const button = document.getElementById('fetch-btn');
+const output = document.getElementById('output');
+
+async function getData() {
+    output.innerText = "Loading...";
+
+    let { data, error } = await supabase
+        .from('items')
+        .select('name');
+
+    if (error) {
+        output.innerText = "Error: " + error.message;
+        return;
+    }
+
+    if (data  && data.length > 0) {
+        output.innerHTML = data.map(item => `<p> ${item.name}</p>`).join('');
+    } else {
+        output.innerText = "No data found. Did you insert a row yet?";
+    }
+}
+
+button.addEventListener('click', getData);
+

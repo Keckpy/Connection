f840ed5fabfc42ffcc1370caef211e15723303bc
Keckpy <36435107+Keckpy@users.noreply.github.com>
2026-08-14 23:09:10 -0400

    Update vite.config.js

--- project/vite.config.js
+++ project/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // 🌟 Tells Vite your repo name so it routes assets correctly on GitHub Pages
-  base: '/Connection/', 
+  base: '/List/', 
 });
